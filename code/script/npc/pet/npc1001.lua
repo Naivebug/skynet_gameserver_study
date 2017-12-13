08edbb2ef500c1f5e2534eb84b818802b6c0ea0f
Naivebug <xiaotiekeng@163.com>
2017-12-13 10:46:37 +0800

    modify

---
+++ code/script/npc/pet/npc1001.lua
@@ -0,0 +1,21 @@
+--# coding:utf-8
+--# 以上系统生成,请忽修改哦!
+--# 以下系统导表数据生成,请忽手动修改-sysytem_data_start
+local _self={}
+function _self.create(self)
+	if not self then self=Cpet() end
+	self.m_SID = 1001
+	self.m_Name ='曹操'
+	self.m_HP = 100
+	self.m_BaseAtk = 300
+	self.m_list1 = {10,20,30,}
+	self.m_map1 = {
+[10] = 20,
+[20] = 30,}
+
+--# 以上系统导表数据生成,请忽手动修改-sysytem_data_end
+--# 以下系统导表数据生成,请忽手动修改-sysytem_auto_file_return_self_start
+	return self end
+return _self
+--# 以上系统导表数据生成,请忽手动修改-sysytem_auto_file_return_self_end
+
---
+++ code/script/npc/pet/npc1002.lua
@@ -0,0 +1,19 @@
+--# coding:utf-8
+--# 以上系统生成,请忽修改哦!
+--# 以下系统导表数据生成,请忽手动修改-sysytem_data_start
+local _self={}
+function _self.create(self)
+	if not self then self=Cpet() end
+	self.m_SID = 1002
+	self.m_Name ='刘备'
+	self.m_HP = 100
+	self.m_BaseAtk = 300
+	self.m_list1 = {[[aa]],[[bb]],}
+	self.m_map1 = {aa = [[bb]],c = 100,}
+
+--# 以上系统导表数据生成,请忽手动修改-sysytem_data_end
+--# 以下系统导表数据生成,请忽手动修改-sysytem_auto_file_return_self_start
+	return self end
+return _self
+--# 以上系统导表数据生成,请忽手动修改-sysytem_auto_file_return_self_end
+
